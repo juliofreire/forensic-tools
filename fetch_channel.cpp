0267be0320f2b1fd6379c2f292ce57da84d1fd2f
Sia <siaitep@gmail.com>
2024-05-24 12:12:18 -0300

    fetch_channel.cpp is working properly

--- fetch_channel.cpp
+++ fetch_channel.cpp
@@ -15,6 +15,57 @@ string binaryToHex(const char* buffer, int length){
 	return oss.str();
 }
 
+string rangeInSeconds(const string date, int sizeRange){
+	string substrdate = date.substr(0,2);
+	ostringstream oss;
+	int seconds = stol(substrdate, 0, 16);
+	oss << hex << seconds+sizeRange << endl;
+	substrdate = oss.str()[0];
+	substrdate += oss.str()[1];
+	substrdate.append(date.substr(2,8));
+
+	return substrdate;
+}
+
+string takeDates(const string data, const int length){
+	string subdata;
+	for (int i = 0; i<length-2; i+=9){
+		subdata = data.substr(i,8);
+		cout << subdata << endl;
+	}
+	return subdata;
+}
+
+string searchChannels(const string date, const string bufferHex, const int length, const int sizeRange){
+	int index;
+	bool found = false;
+	int j = (sizeRange/2)*(-1);
+	while(j <= sizeRange/2){
+		string date_tmp = rangeInSeconds(date, j);
+		for (int i = 0; i < 2*length; i++){
+		string substr = bufferHex.substr(i, 8);
+			if (memcmp(substr.c_str(), date_tmp.c_str(), 8) == 0){
+				// cout << substr << endl;
+				found = true;
+				cout << "Find for date:" << date_tmp << endl;
+				index = i;
+				string channel;
+				// channel = bufferHex[index-22];
+				// channel += bufferHex[index-21];
+				cout << "The channel is:";
+
+				cout << bufferHex[index-22];
+				cout << bufferHex[index-21] << endl;
+				return channel;
+				// break;
+			}
+		}
+	if(found) break;
+	j++;	
+	}
+	return NULL;
+}
+
 
 int main (int argc, char* argv[]){
 
@@ -23,53 +74,49 @@ int main (int argc, char* argv[]){
 
 	ifstream inputfile(inputfile_path, ifstream::binary);
 	ifstream comparefile(comparefile_path);
-	// cout << inputfile.good();
-	if (inputfile.good()){
-		inputfile.seekg(0, inputfile.end);
-		int length1 = inputfile.tellg();
-		inputfile.seekg(0, inputfile.beg);
-
-		char* buffer1 = new char [length1];
-		char* buffer2 = new char [length1];
-		inputfile.read(buffer1, length1);
-
-		string bufferHex = binaryToHex(buffer1, length1);
-
-		string date("5e68725e");
-		int index;
-
-		for (int i = 0; i < 2*length1; i++){
-			string substr = bufferHex.substr(i, 8);
-			if (memcmp(substr.c_str(), date.c_str(), 8) == 0){
-				cout << substr;
-				cout << "achou???";
-				index = i;
-				break;
-			}
+	if inputfile.good(){
+		if (comparefile.good()){
+		} else{
+			cout << "Could'nt read the comparefile" << endl;
+			return 1;
 		}
-
-		cout << bufferHex[index-22];
-		cout << bufferHex[index-21];
-
+	} else {
+		cout << "Could'nt read the inputfile" << endl;
+		return 1;
 	}
 
-	if (comparefile.good()){
-		comparefile.seekg(0, comparefile.end);
-		int length2 = comparefile.tellg();
-		comparefile.seekg(0, comparefile.beg);
+	inputfile.seekg(0, inputfile.end);
+	int length1 = inputfile.tellg();
+	inputfile.seekg(0, inputfile.beg);
 
-		char* buffer2 = new char [length2];
-		comparefile.read(buffer2, length2);
+	char* buffer1 = new char [length1];
+	char* buffer2 = new char [length1];
+	inputfile.read(buffer1, length1);
 
-		string data(buffer2);
-		cout << endl;
-		cout << length2;
-		cout << endl;
-		for (int i = 0; i<length2-2; i+=9){
-			string subdata = data.substr(i,8);
-			cout << subdata << endl;
-		}
+	string bufferHex = binaryToHex(buffer1, length1);
+
+	int sizeRange = 8;
+
+	// int index;
+	// searchChannels(date, bufferHex, length1, 8);
+
+	comparefile.seekg(0, comparefile.end);
+	int length2 = comparefile.tellg();
+	comparefile.seekg(0, comparefile.beg);
+
+	char* buffer2 = new char [length2];
+	comparefile.read(buffer2, length2);
+
+	string data(buffer2);
+	cout << endl;
+	cout << length2;
+	cout << endl;
+	for (int i = 0; i<length2-2; i+=9){
+		string subdata = data.substr(i,8);
+		searchChannels(subdata, bufferHex, length1, 8);
+		cout << subdata << endl;
 	}
+	
 
 	return 0;
 
